cc81b37cc78bc52635fe6fdb1d6208bb107977bb
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-03-19 14:29:19 +0100

    Fix github test action

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, getByDisplayValue, fireEvent, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App', async () => {
+
+  render(<App />)
+
+  screen.getByLabelText(/day/i);
+  screen.getByLabelText(/hour/i);
+  screen.getByLabelText(/minute/i);
+  screen.getByLabelText(/second/i);
+
+  const dayInput = getByDisplayValue(
+    screen.getByLabelText(/day/i).parentElement!,
+    '0'
+  );
+
+  const hourInput = getByDisplayValue(
+    screen.getByLabelText(/hour/i).parentElement!,
+    '00'
+  );
+
+  fireEvent.change(dayInput, { target: {value: '1'} });
+  fireEvent.change(hourInput, { target: {value: '2'} });
+
 });
