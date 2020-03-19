b479029c8acb5457a690e9175d495576e63b74c6
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-03-19 12:09:11 +0100

    Add and play a little bit with PatternFly v4
    npm install @patternfly/react-core @patternfly/react-table @patternfly/react-icons --save

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Button, TextInput, Form } from '@patternfly/react-core';
+import { Table, TableHeader, TableBody } from '@patternfly/react-table';
+
 function App() {
+
+  const cells = [
+    { title: 'Lab #' },
+    { title: 'Lab time' },
+  ];
+
+  const rows = [
+    { cells: [ '1', '123:12' ] },
+    { cells: [ '2', '123:12' ] },
+    { cells: [ '3', '123:12' ] },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Form>
+        <TextInput type="time" css="" onChange={() => console.warn('x')} />
+
+        <Button variant="primary">Start</Button>
+        <Button variant="primary">Stop</Button>
+        <Button variant="primary">Pause</Button>
+        <Button variant="primary">Resume</Button>
+      </Form>
+
+      <Table cells={cells} rows={rows}>
+        <TableHeader />
+        <TableBody />
+      </Table>
+
     </div>
   );
 }
