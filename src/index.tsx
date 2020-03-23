4b01ba3d0356c1486be771e0a103a669f8189f34
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-03-23 02:53:14 +0100

    Remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import '@patternfly/react-core/dist/styles/base.css';
 
-import './index.css';
-
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
