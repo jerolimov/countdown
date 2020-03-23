27be26559854e9a1a524a9d27edf84fc69e64f21
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-03-23 10:23:21 +0100

    Add useMemo

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export default function useModal() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const open = () => setIsOpen(true);
-  const close = () => setIsOpen(false);
+  const open = useMemo(() => () => setIsOpen(true), []);
+  const close = useMemo(() => () => setIsOpen(false), []);
 
   return {
     isOpen,
